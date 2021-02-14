85f2f0fd5d59b2ad9a3a7235212d4ee403fdf366
jefishub <jere.manni@gmail.com>
2021-02-14 05:48:54 +0200

    basic functions working, requires finetuning. ProductBox not tested yet

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ProductBox from './ProductBox.jsx'
 
 const CORS_URL = "https://cors-anywhere.herokuapp.com/" //TO fix CORS error
 const CATEGORY_API_URL = CORS_URL + "https://bad-api-assignment.reaktor.com/v2/products/" //ADD category name
-const MANUFACTURER_API_URL = "https://bad-api-assignment.reaktor.com/v2/availability/" //ADD manufacturer name
+const MANUFACTURER_API_URL = CORS_URL + "https://bad-api-assignment.reaktor.com/v2/availability/" //ADD manufacturer name
 
 function App() {
 
@@ -12,9 +12,6 @@ function App() {
   const [gloves, setGloves] = useState('Gloves not ready')
   const [facemasks, setFacemasks] = useState('Facemasks not ready')
   const [beanies, setBeanies] = useState('Beanies not ready')
-
-  /* Manufacturers per product */
-  const [manufacturersList, setManufacturersList] = useState([]);
  
   /* Manufacturer data object dict */
   const [productAvailabilityData, setProductAvailabilityData] = useState([])
@@ -29,41 +26,54 @@ function App() {
     else if (item === "beanies") {setShowValues(JSON.stringify(beanies))}
   }
 
-  const getManufacturers = () => {
+  /* finds all distinct manufacturers from gloves, facemasks and beanies */
+  const getManufacturers = (products) => {
+    console.log("***products****");
+    console.log(products);
     var manufacturers = [];
-    gloves.map(item => manufacturers.push(item.manufacturer));
-    facemasks.map(item => manufacturers.push(item.manufacturer));
-    beanies.map(item => manufacturers.push(item.manufacturer));
+    products.map(
+      (product) => product.map(item => manufacturers.push(item.manufacturer))
+    )
     var manufacturers_list = new Set(manufacturers);
-    setManufacturersList(manufacturers_list);
+    return Array.from(manufacturers_list);
   }
 
-  const getProductAvailabilityData = () => {
+  const getProductAvailabilityData = (manufacturers_list) => {
+    console.log("***manufacturers_list****");
+    console.log(manufacturers_list);
     var data_list = [];
-    manufacturersList.map( async (item) => {
+    manufacturers_list.map( async (item) => {
+      console.log(item)
       const data = await fetch(MANUFACTURER_API_URL + item);
-      data_list.push({item:data});
+      const datajson = await data.json();
+      data_list.push({item:datajson});
     })
+    console.log("*****data_list****")
+    console.log(data_list);
     setProductAvailabilityData(data_list);
   }
 
-  useEffect(async () => {
-    const [glovesData, facemasksData, beaniesData] = await Promise.all([
-      fetch(CATEGORY_API_URL + "gloves"),
-      fetch(CATEGORY_API_URL + "facemasks"),
-      fetch(CATEGORY_API_URL + "beanies")
+  useEffect(() => {
+    async function getData() {
+      const [glovesData, facemasksData, beaniesData] = await Promise.all([
+        fetch(CATEGORY_API_URL + "gloves"),
+        fetch(CATEGORY_API_URL + "facemasks"),
+        fetch(CATEGORY_API_URL + "beanies")
     ])
-
     const glovesjson = await glovesData.json();
     const facemasksjson = await facemasksData.json();
     const beaniesjson = await beaniesData.json();
 
     setGloves(glovesjson);
     setFacemasks(facemasksjson);
-    setBeanies(beaniesjson);
+    setBeanies(beaniesjson); 
+
+    getProductAvailabilityData(
+      getManufacturers([glovesjson, facemasksjson, beaniesjson])
+      ); 
+    }
+    getData();
     setShowValues("Data loaded");
-    await getManufacturers();
-    await getProductAvailabilityData();
   },[reload])
 
   const changeReload = () => {
@@ -75,9 +85,9 @@ function App() {
   return (
     <div>
       <button onClick={() => changeReload()}>Reload data</button>
-      <button onClick={() => showItem("gloves")}>Gloves</button>
-      <button onClick={() => showItem("facemasks")}>Facemasks</button>
-      <button onClick={() => showItem("beanies")}>Beanies</button>
+      <button onClick={() => showItem(<ProductBox product_data={gloves} availability_data={productAvailabilityData} /> )}>Gloves</button>
+      <button onClick={() => showItem(<ProductBox product_data={facemasks} availability_data={productAvailabilityData} /> )}>Facemasks</button>
+      <button onClick={() => showItem(<ProductBox product_data={beanies} availability_data={productAvailabilityData} /> )}>Beanies</button>
       <p>{ showValues }</p>
     </div>
   );
